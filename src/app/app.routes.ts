15608ef924e5934e9c4efdfa0ba21f3b059bcfb9
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-04-11 21:17:53 +0200

    [Update] Hover Animation for films

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WinnersPageComponent } from './pages/winners-page/winners-page.component';
+import { FilmComponent } from './pages/film/film.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta vacía dirige al componente HomeComponent
+  { path: 'winners-page', component: WinnersPageComponent }, // Ruta '/winners-page' dirige al componente WinnerPageComponent
+  { path: 'film/:id', component: FilmComponent }, // Ruta '/film/:id' dirige al componente FilmComponent, donde ':id' es un parámetro dinámico
+];
---
+++ src/app/pages/winners-page/winners-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-winners-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './winners-page.component.html',
+  styleUrl: './winners-page.component.css'
+})
+export class WinnersPageComponent {
+
+}
