5836eb65b73a007b46104dd5f33e1c0710d79f57
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-05-17 00:52:54 +0200

    [Update] Film photos and detials

--- src/app/components/film/film.component.ts
+++ src/app/components/film/film.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Film } from './film';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-film',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './film.component.html',
   styleUrl: './film.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FilmComponent } from '../../components/film/film.component';
 import { Film } from '../../components/film/film';
 import { FilmService } from '../../services/film.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FilmComponent],
+  imports: [FilmComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
