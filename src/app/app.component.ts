359643bf1d12d096c89f289083a5e9de9132952d
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-05-27 00:34:57 +0200

    [Update] Added footer and FilmComponent details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, RouterLink],
+  imports: [RouterOutlet, NavBarComponent, RouterLink, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
